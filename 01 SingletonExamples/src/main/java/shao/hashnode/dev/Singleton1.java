9ebd1e851369f363b5875f9ab50ca4c83ea80f56
Claude Shao <shadhunt@gmail.com>
2021-07-03 00:36:56 -0400

    Added Thread safe. Added mutli Thread test cases.

--- 01 SingletonExamples/src/main/java/shao/hashnode/dev/Singleton1.java
+++ 01 SingletonExamples/src/main/java/shao/hashnode/dev/Singleton1.java
@@ -1,17 +1,28 @@
 package shao.hashnode.dev;
 
-final class Singleton1 implements SingletonInterface{       //'final' prevent inner class created
+public final class Singleton1 implements SingletonInterface{       //'final' prevent inner class created
 	private static Singleton1 singletonObj;      //'static' prevent more than one object created
 
 	private Singleton1() {          //'private of constructor prevent subclasses inherit the constructor and prevent new the object
-		 
 	}
 	
-	public static Singleton1 getInstance()
+	public synchronized static Singleton1 getInstance()
 	{
 		if(singletonObj==null)
 		{
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			singletonObj = new Singleton1();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			return singletonObj;
 		}
 		else
--- 01 SingletonExamples/src/main/java/shao/hashnode/dev/TestNGMain.java
+++ 01 SingletonExamples/src/main/java/shao/hashnode/dev/TestNGMain.java
@@ -4,6 +4,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import shao.hashnode.dev.threads.TestThread;
+
 public class TestNGMain {
 
   private SingletonInterface singletonObject1;
@@ -22,10 +24,20 @@ public class TestNGMain {
 		  System.out.println("singletonObj1="+singletonObject1);
 		  System.out.println("singletonObj2="+singletonObject2);
 	  }
+	  
+	  /*
+	   * multi-thread testing case
+	   */
+	  TestThread tt = new TestThread();
+	  Thread thread1 = new Thread( tt, "thread1");
+	  Thread thread2 = new Thread( tt, "thread2");
+	  thread1.start();
+	  thread2.start();
+	  
   }
   @BeforeMethod
   public void beforeMethod() {	  
-//	  singletonObject = new Singleton1(); this doesn't work because the constructor is private
+//	  singletonObject = new Singleton1(); this doesn't work because the constructor is private	  
 	  singletonObject1 = Singleton1.getInstance();
 	  singletonObject2 = Singleton1.getInstance();
 	  
